fb75edb6213d43044ad1aa4d7a3ebc94ecc15b5b
shahbajkhan2023 <rk2403476@gmail.com>
2024-08-09 16:54:50 +0530

    comments added

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.mixins import (
-    LoginRequiredMixin,
-    UserPassesTestMixin # new
-)
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView # new
+from django.views.generic.detail import SingleObjectMixin # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse # new
+from .forms import CommentForm
 from .models import Article
 
 
@@ -13,9 +13,41 @@ class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "article_list.html"
 
 
-class ArticleDetailView(LoginRequiredMixin, DetailView): # new
+
+# for detail view to get form 
+class CommentGet(DetailView): # new
     model = Article
     template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+# for detail view to post form
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class ArticleUpdateView(
@@ -51,6 +83,7 @@ class ArticleCreateView(LoginRequiredMixin ,CreateView): # new
     "body",
     )
 
+    # for set current user in form
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super.form_valid(form)
